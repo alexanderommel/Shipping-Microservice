5393f6d8723e6ba4a4b162ded7ceb71b6cfce847
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2021-12-16 17:24:22 -0500

    DispatchStatusActuator Implementation

---
+++ src/main/java/com/tongue/shippingservice/services/DispatchStatusActuator.java
@@ -0,0 +1,35 @@
+package com.tongue.shippingservice.services;
+
+import com.tongue.shippingservice.domain.Courier;
+import com.tongue.shippingservice.domain.Shipping;
+import com.tongue.shippingservice.repositories.ShippingRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+public class DispatchStatusActuator {
+
+    private CourierSessionHandler sessionHandler;
+    private ShippingRepository shippingRepository;
+
+    public DispatchStatusActuator(@Autowired CourierSessionHandler sessionHandler,
+                                  @Autowired ShippingRepository shippingRepository){
+        this.sessionHandler=sessionHandler;
+        this.shippingRepository=shippingRepository;
+    }
+
+    public Boolean onSuccessfulShippingDispatch(Courier courier){
+        Boolean solved = sessionHandler.updateCourierStatus(Courier.status.BUSY, courier);
+        return solved;
+    }
+
+    public Shipping onInternalErrorShippingDispatch(Shipping shipping){
+        shipping.setStatus(Shipping.Status.CANCELLED_BY_SYSTEM);
+        shipping.setDriver(null);
+        shippingRepository.save(shipping);
+        return shipping;
+    }
+
+}
