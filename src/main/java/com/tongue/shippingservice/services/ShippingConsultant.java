6520f6d5f11287cdd8a0cd55049b1b65896da2ed
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2021-12-16 23:40:23 -0500

    ShippingConsultant Implementation

---
+++ src/main/java/com/tongue/shippingservice/services/ShippingConsultant.java
@@ -0,0 +1,33 @@
+package com.tongue.shippingservice.services;
+
+import com.tongue.shippingservice.domain.Position;
+import com.tongue.shippingservice.domain.Shipping;
+import org.springframework.data.geo.CustomMetric;
+import org.springframework.data.geo.Distance;
+import org.springframework.data.geo.Metric;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.LocalTime;
+
+@Service
+public class ShippingConsultant {
+
+    /** Simple responses in order to advance faster with the development **/
+
+    public BigDecimal shippingFee(Position origin, Position destination){
+        return BigDecimal.valueOf(2.25);
+    }
+
+    public Distance tripDistance(Position origin, Position destination){
+        Metric metric = new CustomMetric(1,"km");
+        Distance distance = new Distance(10.5, metric);
+        return distance;
+    }
+
+    public LocalTime arrivalTime(Position origin, Position destination){
+        LocalTime time = LocalTime.of(0,30);
+        return time;
+    }
+
+}
