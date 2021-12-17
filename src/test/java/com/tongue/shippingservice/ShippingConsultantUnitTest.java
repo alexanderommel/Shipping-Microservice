76861252179b4fe89fcf7056164544f83bb3efa0
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2021-12-16 23:40:59 -0500

    Refactor ShippingConsultantUnitTest

--- src/test/java/com/tongue/shippingservice/ShippingConsultantUnitTest.java
+++ src/test/java/com/tongue/shippingservice/ShippingConsultantUnitTest.java
@@ -3,8 +3,10 @@ package com.tongue.shippingservice;
 import com.tongue.shippingservice.domain.Position;
 import com.tongue.shippingservice.services.ShippingConsultant;
 import org.junit.jupiter.api.Test;
+import org.springframework.data.geo.Distance;
 
 import java.math.BigDecimal;
+import java.time.LocalTime;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -22,25 +24,26 @@ public class ShippingConsultantUnitTest {
 
     @Test
     public void givenOriginAndDestinationPositions_whenEstimatingDistance_thenDistanceIsAroundToExpected(){
-        Double expected = 15000.0; // Meters
-        Double limit = 1000.0;
+        Double expected = 10.0; // Meters
+        Double limit = 10.10;
         ShippingConsultant consultant = new ShippingConsultant();
         Position origin = Position.builder().latitude("125.11").longitude("98.102").build();
         Position destination = Position.builder().latitude("211.5").longitude("205.25").build();
-        Double distance = consultant.tripDistance(origin,destination);
-        Double difference = Math.abs(expected-distance);
+        Distance distance = consultant.tripDistance(origin,destination);
+        Double difference = Math.abs(expected-distance.getValue());
         Boolean delta = difference>limit;
         assertFalse(delta);
     }
 
+    @Test
     public void givenOriginAndDestinationPositions_whenEstimatingTime_thenExpectedTimeIsAroundExpected(){
-        Double expected = 50.0; // Minutes
+        Double expected = 40.0; // Minutes
         Double limit = 20.0;
         ShippingConsultant consultant = new ShippingConsultant();
         Position origin = Position.builder().latitude("125.11").longitude("98.102").build();
         Position destination = Position.builder().latitude("211.5").longitude("205.25").build();
-        Double time = consultant.arrivalTime(origin,destination);
-        Double difference = Math.abs(expected-time);
+        LocalTime time = consultant.arrivalTime(origin,destination);
+        Double difference = Math.abs(expected-time.getMinute());
         Boolean delta = difference>limit;
         assertFalse(delta);
     }
