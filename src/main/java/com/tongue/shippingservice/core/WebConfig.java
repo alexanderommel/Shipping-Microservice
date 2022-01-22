25a44a55c287c5c20774c53f2f25381ed37f11c0
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-01-21 23:58:23 -0500

    Cors Support

--- src/main/java/com/tongue/shippingservice/core/WebConfig.java
+++ src/main/java/com/tongue/shippingservice/core/WebConfig.java
@@ -4,17 +4,19 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @Slf4j
 @EnableWebMvc
-public class WebConfig extends WebMvcConfigurerAdapter {
+public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        log.info("Cors enabled for all originsx");
+        log.info("Cors enabled for all origins");
         registry.addMapping("/**")
-        .allowedOrigins("http://localhost:8081").allowedMethods("GET","POST","OPTIONS").allowCredentials(true);
+                .allowedOrigins("http://localhost:8080/","http://localhost:8081/","http://localhost:8081")
+                .allowedMethods("GET","POST","OPTIONS","PUT","PATCH","DELETE").allowCredentials(true);
     }
 }
