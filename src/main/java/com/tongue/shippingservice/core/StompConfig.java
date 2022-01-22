2fd4e79b1d07791ef1b992d682521d2d11915619
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-01-22 00:01:19 -0500

    Cors Enabled for Stomp

--- src/main/java/com/tongue/shippingservice/core/StompConfig.java
+++ src/main/java/com/tongue/shippingservice/core/StompConfig.java
@@ -1,10 +1,16 @@
 package com.tongue.shippingservice.core;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
@@ -13,18 +19,55 @@ import org.springframework.web.socket.server.HandshakeInterceptor;
 import java.util.Map;
 
 @Configuration
+@Slf4j
 @EnableWebSocketMessageBroker
 public class StompConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/connect").withSockJS();
+        registry.addEndpoint("/ws")
+                .setAllowedOrigins("http://localhost:8081",
+                        "http://localhost:8081/",
+                        "http://localhost:8080",
+                        "http://localhost:8080/")
+                .setAllowedOriginPatterns("/**")
+                ;
     }
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry registry) {
-        registry.enableSimpleBroker("/topics","/queue");
         registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/topics","/queue");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.setInterceptors(channelInterceptor());
+    }
+
+    private ChannelInterceptor channelInterceptor(){
+        return new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                log.info(message.toString());
+                return ChannelInterceptor.super.preSend(message, channel);
+            }
+        };
+    }
+
+    private HandshakeInterceptor getInterceptor(){
+        return new HandshakeInterceptor() {
+            @Override
+            public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Map<String, Object> attributes) throws Exception {
+                log.info("Pre Handshake");
+                return true;
+            }
+
+            @Override
+            public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler, Exception exception) {
+                log.info("Successful handshake");
+            }
+        };
     }
 
 }
