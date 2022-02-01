75dced8f35aa9e7bf261343949380b3671d5def2
alexanderommel <rommel.valdiviezo@epn.edu.ec>
2022-01-31 23:58:16 -0500

    Position must implement serializable to be added as redis session attribute

--- src/main/java/com/tongue/shippingservice/domain/Position.java
+++ src/main/java/com/tongue/shippingservice/domain/Position.java
@@ -7,13 +7,14 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.geo.Point;
 
 import javax.persistence.Embeddable;
+import java.io.Serializable;
 
 @Data
 @Builder
 @Embeddable
 @AllArgsConstructor
 @NoArgsConstructor
-public class Position implements GeometricPoint{
+public class Position implements GeometricPoint, Serializable {
     private String latitude;
     private String longitude;
     private String address;
@@ -22,5 +23,9 @@ public class Position implements GeometricPoint{
     @Override
     public Point geometricPoint() {
         return new Point(Double.valueOf(latitude),Double.valueOf(longitude));
+    }
+
+    public boolean validLatLongCoordinates(){
+        return true;
     }
 }
